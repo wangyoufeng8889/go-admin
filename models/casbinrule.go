d109ba776c391e170ab3460eda52e676f9b53741
wangyoufeng8889 <wangyoufeng8889@126.com>
2020-09-17 18:04:00 +0800

    修改数据库

--- models/casbinrule.go
+++ models/casbinrule.go
@@ -2,6 +2,7 @@ package models
 
 //casbin_rule
 type CasbinRule struct {
+	Id          int    `json:"Id" gorm:"primary_key;AUTO_INCREMENT"` // 编码
 	PType string `json:"p_type" gorm:"size:100;"`
 	V0    string `json:"v0" gorm:"size:100;"`
 	V1    string `json:"v1" gorm:"size:100;"`
